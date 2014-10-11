23482a6441d736498584ae37c4c32fe2f4ca8959
themythicaldrago <ryan.louie@students.olin.edu>
2014-10-11 10:22:45 -0400

    added models to admin.py, populated db with test info

--- goodbets/admin.py
+++ goodbets/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from goodbets.models import User, Challenge, Bet
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Challenge)
+admin.site.register(Bet)
--- goodbets/models.py
+++ goodbets/models.py
@@ -11,7 +11,11 @@ class User(models.Model):
 	id
 	bets = {bet1, bet2, ...}
 	"""
-	username = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.username
+
+	username = models.CharField(max_length=50)
 
 class Challenge(models.Model):
 	"""
@@ -27,6 +31,11 @@ class Challenge(models.Model):
 	bets: the Bets that have been pooled around this challenge.
 	challengees: the users who have been challenged
 	"""
+
+	def __str__(self):
+		return self.title
+
+	title = models.CharField(default='', max_length=150)
 	description = models.TextField()
 	bets = models.ManyToManyField('Bet')
 	challengees = models.ManyToManyField('User')
@@ -42,7 +51,12 @@ class Bet(models.Model):
 	---------------
 	user: foreign key connecting the user who has put money into this bet
 	amount: dollar value of the bet
-	charity: charity that this bet will be going to.
+	challenge: the challenge this bet is associated with
+	?charity: charity that this bet will be going to.
 	"""
+
+	def __str__(self):
+		return str(self.user) + str(self.amount)
+
 	user = models.ForeignKey('User')
 	amount = models.FloatField(default=0.15)
