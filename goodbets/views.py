7ebf26bacea80522fc241c12f29bddf9928d4421
Rachel Mathew <rachelmathew999@gmail.com>
2014-10-12 14:59:51 -0400

    Adding an actual html file

--- goodbets/views.py
+++ goodbets/views.py
@@ -6,4 +6,4 @@ def index(request):
     return TemplateResponse(request,'index.html')
 
 def profile(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	return TemplateResponse(request,'profile.html')
