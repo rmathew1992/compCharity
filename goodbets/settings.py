32249f694ee00413379953bb81a454bbb5d23440
Kevin O'Toole <kotoole@onshape.com>
2014-10-27 18:39:19 -0400

    changing static root to project

--- goodbets/settings.py
+++ goodbets/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 GOODBETS_ROOT = os.path.abspath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.abspath(os.path.join(GOODBETS_ROOT, 'static'))
+STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_ROOT, 'static'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
