00cf4e323d678c9c96d94628687d81139201e8da
Nitya Dhanushkodi <nitya.dhanushkodi>
2014-11-04 23:31:40 -0500

    profile gets stuff

--- goodbets/views.py
+++ goodbets/views.py
@@ -1,7 +1,7 @@
 
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template.response import TemplateResponse
-from django.shortcuts import get_object_or_404, render, redirect
+from django.shortcuts import get_object_or_404, get_list_or_404, render, redirect
 from goodbets.forms import ChallengeForm
 from django.forms import ModelForm
 from goodbets.models import User, Challenge, Bet
@@ -13,9 +13,11 @@ def index(request):
 def profile(request):
     user_list = User.objects.all()
     challenge_list = Challenge.objects.all()
+    bet_list = Bet.objects.all()
     context = {
         'user_list': user_list, 
         'challenge_list': challenge_list, 
+        'bet_list': bet_list,
         'request': request,
     }
     return render(request, 'profile.html', context)
