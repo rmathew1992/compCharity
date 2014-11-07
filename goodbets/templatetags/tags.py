10e235bcb344d21d5667972b1f10b4c5240aae8d
Kevin O'Toole <kotoole@onshape.com>
2014-11-06 23:06:25 -0500

    new active tag

--- goodbets/templatetags/tags.py
+++ goodbets/templatetags/tags.py
@@ -6,9 +6,23 @@ logger = logging.getLogger(__name__)
 register = template.Library()
 
 @register.simple_tag
-def active(request, pattern):
-    logger.debug('pattern type %s' % (pattern))
-    if pattern in request.path:
+def active(request, pattern='', view=''):
+    path = request.path[1:] # Strip the leading /
+
+    if view != '':
+        for urlpattern in urls.urlpatterns:
+            # Skip include()s, they don't have the _callback_str attribute.
+            if isinstance(urlpattern, RegexURLResolver):
+                continue
+
+            if view == urlpattern._callback_str:
+                pattern = urlpattern.regex
+                break
+
+    if pattern == '':
+        return ''
+
+    if re.search(pattern, path):
         return 'active'
     else:
         return ''
