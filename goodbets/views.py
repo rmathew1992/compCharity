7ffad6b6f63e69d5eb7846249406cfe2024b2384
Kevin O'Toole <kotoole@onshape.com>
2014-11-20 02:02:31 -0500

    fulfill page

--- goodbets/views.py
+++ goodbets/views.py
@@ -1,4 +1,3 @@
-
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template.response import TemplateResponse
 from goodbets.forms import ChallengeForm, ChipinForm
@@ -111,6 +110,9 @@ def challenge(request):
 
 def home(request):
     return render(request, 'home.html', {'request': request})
+
+def fulfill(request):
+    return render(request, 'fulfill.html', {'request': request})
 
 def about(request):
     return render(request, 'about.html', {'request': request},)
